89de4c679d78e59c2f27de72345f8854a06ecd9e
areeshaFatima <areeshaf7@gmail.com>
2018-10-21 16:38:39 +0530

    game state

--- TicTacToe/app/src/main/java/com/example/hp/tictactoe/MainActivity.java
+++ TicTacToe/app/src/main/java/com/example/hp/tictactoe/MainActivity.java
@@ -2,18 +2,27 @@ package com.example.hp.tictactoe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
-    int activePlayer=0;//0=yellow,1=red
+    int[] game={2,2,2,2,2,2,2,2,2};
+
+    int[][] winningPositions={{0,1,2},{3,4,5},{6,7,8},{0,4,8},{2,4,6},{0,3,6},{1,4,7},{2,5,8}};
+    int activePlayer=0;//0=yellow,1=red,2:Empty
 
     public void dropIn(View view){
 
 
 
         ImageView counter=(ImageView) view;
+
+        Log.i("Tag",counter.getTag().toString());
+
+        int tappedCounter=Integer.parseInt(counter.getTag().toString());
+        game[tappedCounter]=activePlayer;
         counter.setTranslationY(-1500);
         if(activePlayer==0) {
             counter.setImageResource(R.drawable.yellow);
