7487c423eba1019c538fe7d0a081fdb5924ece51
areeshaFatima <areeshaf7@gmail.com>
2018-10-23 01:31:57 +0530

    update

--- TicTacToe/app/src/main/java/com/example/hp/tictactoe/MainActivity.java
+++ TicTacToe/app/src/main/java/com/example/hp/tictactoe/MainActivity.java
@@ -4,7 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,37 +16,47 @@ public class MainActivity extends AppCompatActivity {
     int[][] winningPositions={{0,1,2},{3,4,5},{6,7,8},{0,4,8},{2,4,6},{0,3,6},{1,4,7},{2,5,8}};
     int activePlayer=0;//0=yellow,1=red,2:Empty
 
+    boolean gameActive=true;
+
     public void dropIn(View view){
 
 
 
         ImageView counter=(ImageView) view;
 
-        Log.i("Tag",counter.getTag().toString());
+        //Log.i("Tag",counter.getTag().toString());
 
         int tappedCounter=Integer.parseInt(counter.getTag().toString());
-        game[tappedCounter]=activePlayer;
-        counter.setTranslationY(-1500);
-        if(activePlayer==0) {
-            counter.setImageResource(R.drawable.yellow);
-            activePlayer=1;
+        if(game[tappedCounter]==2 && gameActive) {
+            game[tappedCounter] = activePlayer;
+            counter.setTranslationY(-1500);
+            if (activePlayer == 0) {
+                counter.setImageResource(R.drawable.yellow);
+                activePlayer = 1;
 
-        }else{
-            counter.setImageResource(R.drawable.red);
-            activePlayer=0;
-        }
-        counter.animate().translationYBy(1500).rotation(3600).setDuration(500);
+            } else {
+                counter.setImageResource(R.drawable.red);
+                activePlayer = 0;
+            }
+            counter.animate().translationYBy(1500).rotation(3600).setDuration(500);
 
-        for(int[] winningpos:winningPositions){
-            if(game[winningpos[0]]==game[winningpos[1]] && game[winningpos[1]]==game[winningpos[2]] && game[winningpos[0]]!=2){
+            for (int[] winningpos : winningPositions) {
+                if (game[winningpos[0]] == game[winningpos[1]] && game[winningpos[1]] == game[winningpos[2]] && game[winningpos[0]] != 2) {
 
-                String winner="";
-                if(activePlayer==1){
-                    winner="YELLOW";
-                }else{
-                    winner="RED";
+                    gameActive=false;
+                    String winner = "";
+                    if (activePlayer == 1) {
+                        winner = "YELLOW";
+                    } else {
+                        winner = "RED";
+                    }
+                    //Toast.makeText(this, winner + " has won!!", Toast.LENGTH_SHORT).show();
+                    Button playAgainButton=(Button)findViewById(R.id.button);
+                    TextView winnerTextView=(TextView)findViewById(R.id.textView2);
+                    winnerTextView.setText(winner+ " has Won!!");
+                    winnerTextView.setVisibility(View.VISIBLE);
+                    playAgainButton.setVisibility(View.VISIBLE);
                 }
-                Toast.makeText(this, winner+" has won!!", Toast.LENGTH_SHORT).show();
             }
         }
     }
